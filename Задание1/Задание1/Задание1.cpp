d5db596f887084756cbf0d617bc005fc2f2eb575
dvrnkvv <74918938+dvrnkvv@users.noreply.github.com>
2022-05-06 10:54:53 +0300

    Zadaniye1_2

---
+++ Задание1/Задание1/Задание1.cpp
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <conio.h>
+#include <clocale>
+using namespace std;
+int main()
+{
+	double x, y;
+	bool c;
+	setlocale(LC_ALL, "Russian_Russia.1251");
+	cout << "x= ";
+	cin >> x;
+	cout << "y= ";
+	cin >> y;
+	if ((x * x + y * y) < 0 && y<1 && y>-1 && x<0 && x>-1)
+		cout << "Точка попала в область C" << endl;
+	else
+		if (x < -1 && y<1 && y>-1)
+			cout << "Точка попала в область D" << endl;
+		else
+			if (y > 1 && x < 2)
+				cout << "Точка попала в область G" << endl;
+			else
+				if (y < -1 && x - 1 < -1)
+					cout << "Точка попала в область K" << endl;
+				else
+					if ((x * x + y * y) > 1 && x - 1 < 1)
+						cout << "Точка попала в область E" << endl;
+					else
+						if ((x * x + y * y) < 1 && x > 0 && x - 1 > -1)
+							cout << "Точка попала в область A" << endl;
+						else
+							if ((x * x + y * y) < 1 && x - 1 < 0)
+								cout << "Точка попала в область F" << endl;
+							else
+								cout << "Точка попала в область B" << endl;
+	
+		
+	return 0;
+}
---
+++ Задание2/Задание2/Задание2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <clocale>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int N,c,k,o;
+	cout << "Введите число: " ;
+	cin >> N;
+	c = 4;
+	k = N / c;
+	o = N % c;
+	if (o == 0)
+		cout << "N = " <<c <<" * " << k << endl;
+	else
+		cout <<"N = " <<c <<" * " <<k<< " + " <<o << endl;
+	return 0;
+
+
+
+	
+}
