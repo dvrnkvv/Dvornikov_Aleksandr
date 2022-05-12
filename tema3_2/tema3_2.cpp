6aca40cf0b37b29448e26805e60723c6fce42e8a
dvrnkvv <74918938+dvrnkvv@users.noreply.github.com>
2022-05-13 00:33:56 +0300

    Add files via upload

---
+++ tema3_2/tema3_2.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include <clocale>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+	int n, s;
+	cout << "n= ";
+	cin >> n;
+	s = 0;
+	while(n>0)
+	{
+		s += n % 10;
+		n /= 10;
+	}
+	cout << s << endl;
+	return 0;
+
+}
