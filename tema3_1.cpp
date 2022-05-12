f6810eaced5de833bc71a4edcb9e5b518bad1e97
dvrnkvv <74918938+dvrnkvv@users.noreply.github.com>
2022-05-13 00:21:09 +0300

    Add files via upload

---
+++ tema3_1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include<clocale>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	int a, b,sum;
+	setlocale(LC_ALL, "Rus");
+	for (int i = 1; i <= 30; i++)
+	{
+		if (i % 2 == 0)
+			a = i;
+		else
+			a = (i / 2);
+		if (i % 2 == 0)
+			b = i * i;
+		else
+			b = i * i * i;
+		sum = pow((a - b), 2);
+	}
+	cout << sum << endl;
+	return 0;
+}
