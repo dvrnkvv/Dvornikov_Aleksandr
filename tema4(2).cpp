8fba8b52b81edcbffe21e459dc2c379f9458bb31
dvrnkvv <74918938+dvrnkvv@users.noreply.github.com>
2022-05-28 16:27:14 +0300

    Add files via upload

---
+++ tema4(2).cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <locale>
+using namespace std;
+int sum(int a)
+{
+    int b = 0;
+    for (int i = 1; i < a; i++)
+    {
+        if (a % i == 0)
+            b += i;
+    }
+    return b;
+}
+
+void main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int n;
+    cout << "Введите число: "; cin >> n;
+    while (n <= sum(n))
+    {
+        if (n == sum(n))
+        {
+            cout << "Число " << n << " совершенное";
+        }
+        else
+        {
+            cout << "Число " << n << " не совершенное" << endl;
+            int sum = 0;
+            for (int i = 1; i < n; i++)
+            {
+                sum = 0;
+                for (int j = 1; j < i / 2 + 1; j++)
+                {
+                    if (i % j == 0)
+                        sum += j;
+                }
+
+                if (sum == i)
+                    cout << "Cовершенное число  " << i << endl;
+
+            }
+        }
+    }
+}
---
+++ tema4.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <cmath>
+#include <clocale>
+using namespace std;
+int stepen(int x, int k);
+int factorial(int k);
+void main()
+{
+	setlocale(LC_ALL, "Russain");
+	int x, k = 0, s, sum = 0, e;
+	cout << "Vvedite argument = ";
+	cin >> x;
+	cout << "Vvedite tochnost = ";
+	cin >> e;
+	int kk = 1;
+	while (true)
+	{
+		s = stepen(-1, k - 1) * stepen(x, 2 * k + 1) / (factorial(2 * k - 1) * factorial(2 * k + 1));
+		if (k)
+		{
+			if (fabs(s) < e) {
+				break;
+			}
+			kk *= k;
+		}
+		sum += s;
+		k++;
+	}
+	cout << "rezultat = " << sum << endl;
+}
+int stepen(int x, int k)
+{
+	int p;
+	p = pow(x, k);
+	return p;
+}
+int factorial(int k)
+{
+	int res = 1;
+	for (int i = 1; i <= k; i++)
+	{
+		res = res * i;
+	}
+	return res;
+}
